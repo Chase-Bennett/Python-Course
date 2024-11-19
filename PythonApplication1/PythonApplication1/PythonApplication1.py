7d323a9166f38aeceef02806b3216bd76687c515
Chase-Bennett <bennettchase638@gmail.com>
2024-11-19 10:33:38 -0500

    2.3, Int Float String testing

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -6,3 +6,20 @@ space = str("DOG")
 print("hissssssss\n"+ space)
 
 
+print(0o123)
+
+
+
+print(0.0000000000000000000001)
+
+print("I like \"Monty Python\"")
+print('I\'m Monty Python.')
+print("I\'m Monty Python.")
+
+
+print(True > False)
+print(True < False)
+ 
+
+print('"I\'m"'+ '\n""Learning""'+'\n"""Python"""')
+print(2**3)
