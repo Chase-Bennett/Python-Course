e353e099e2c025aea5aa5ef3df7d2564592cde9d
Chase-Bennett <bennettchase638@gmail.com>
2024-11-20 11:36:44 -0500

    Variables
    a lot is commented out

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,4 +1,7 @@
 
+from ast import UAdd
+
+
 space = str("DOG")
 
 
@@ -22,7 +25,7 @@ print(True < False)
  
 
 print('"I\'m"'+ '\n""Learning""'+'\n"""Python"""')
-print(2**3)
+"""print(2**3)
 
 print( 12//4.5)
 print( 12%4.5)
@@ -40,3 +43,36 @@ print(2 ** 2 ** 3)
 
 print("when he operations are on the same priority, then go left to right \n 2 * 3 % 5")
 print(2 * 3 % 5)
+
+
+""" 
+
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+while(var<=10):
+    
+    print(var)
+    var = var + 1
+    var=var**2
+    print(var)
+    
+
+ 
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+
+
+john = 3
+mary = 5
+adam = 6
+print( str(john) + "," + str(mary) + "," + str(adam))
+print(john, mary, adam, sep=',')
+total_Apples = john + mary + adam
+print(total_Apples)
