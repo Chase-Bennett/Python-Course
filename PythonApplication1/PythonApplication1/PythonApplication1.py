7162fb3fa2b684ce6b1ac208b5b37532219b9ad1
Chase-Bennett <bennettchase638@gmail.com>
2024-11-21 11:17:15 -0500

    Input + concatenation

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -120,6 +120,25 @@ print("y =", y)
 print("Tell me anything...")
 anything = input()
 print("Hmm...", anything, "... Really?")
-"""
+
+
 anything = input("Tell me anything...")
 print("Hmm...", anything, "...Really?")
+
+
+anything = float(input("Enter a number: "))
+something = anything ** 2.0
+print(anything, "to the power of 2 is", something)
+
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+print("Hypotenuse length is", (leg_a**2 + leg_b**2) ** .5)
+"""
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
+
+print("+" + 10 * "-" + "+")
+print(("|" + " " * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
