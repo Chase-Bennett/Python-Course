eb7dc2214207d38d5280becf368d81b45ae4297a
Chase-Bennett <bennettchase638@gmail.com>
2024-12-02 10:50:48 -0500

    Flower code and largest number

--- Unit 3/Unit 3/Unit 3/Unit 3, solution/Unit 3/Unit_3.py
+++ Unit 3/Unit 3/Unit 3/Unit 3, solution/Unit 3/Unit_3.py
@@ -1,5 +1,5 @@
 
-
+"""
 
 #n = int(input("num:"))
 #print( n >= 100) 
@@ -11,10 +11,60 @@ number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
  
 # Choose the larger number
-if number1 > number2:
-    larger_number = number1
+if number1 > number2: larger_number = number1
+   
 else:
     larger_number = number2
  
 # Print the result
 print("The larger number is:", larger_number)
+
+
+
+
+
+
+# Read three numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+ 
+# We temporarily assume that the first number
+# is the largest one.
+# We will verify this soon.
+largest_number = number1
+ 
+# We check if the second number is larger than the current largest_number
+# and update the largest_number if needed.
+if number2 > largest_number:
+    largest_number = number2
+ 
+# We check if the third number is larger than the current largest_number
+# and update the largest_number if needed.
+if number3 > largest_number:
+    largest_number = number3
+ 
+# Print the result
+print("The largest number is:", largest_number)
+ 
+
+
+input = input("what kills harmfull objects: ")
+
+if input == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+
+elif input == "spathiphyllum":
+  print("No, I want a big Spathiphyllum!")
+
+else:
+    print("Spathiphyllum! Not "+ input);
+    
+    """
+
+
+
+
+
+
+
