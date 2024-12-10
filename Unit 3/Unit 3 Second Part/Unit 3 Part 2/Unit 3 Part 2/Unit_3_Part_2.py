8771d15018f57a3eeeab486f77b0621f1d435e0a
Chase-Bennett <bennettchase638@gmail.com>
2024-12-10 11:08:39 -0500

    Bitwise Operators

--- Unit 3/Unit 3 Second Part/Unit 3 Part 2/Unit 3 Part 2/Unit_3_Part_2.py
+++ Unit 3/Unit 3 Second Part/Unit 3 Part 2/Unit 3 Part 2/Unit_3_Part_2.py
@@ -1,1 +1,18 @@
-print("whatst")
+j = 22
+i=15 
+
+
+
+bit = i &j
+print(bit)
+
+bitneg = ~i
+print(bitneg)
+print(11111)
+
+
+var = 17
+var_right = var >> 1
+var_left = var << 2
+print(var, var_left, var_right)
+
