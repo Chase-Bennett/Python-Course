46df5eac242a790d83f88aec35944ee03111def8
Chase-Bennett <bennettchase638@gmail.com>
2024-12-11 10:45:47 -0500

    update and testing

--- Unit 3/Unit 3 Second Part/Unit 3 Part 2/Unit 3 Part 2/Unit_3_Part_2.py
+++ Unit 3/Unit 3 Second Part/Unit 3 Part 2/Unit 3 Part 2/Unit_3_Part_2.py
@@ -1,3 +1,6 @@
+from tokenize import Number
+
+"""
 j = 22
 i=15 
 
@@ -15,4 +18,16 @@ var = 17
 var_right = var >> 1
 var_left = var << 2
 print(var, var_left, var_right)
+numbers = [10, 5, 7, 2, 1]
+print(numbers)
+"""
 
+print("Source file edit test")
+print(".sln edit test")
+
+
+numbers = [10, 5, 7, 2, 1]
+print("Original list contents:", numbers)  # Printing original list contents.
+ 
+numbers[0] = 111
+print("New list contents: ", numbers)  # Current list contents.
