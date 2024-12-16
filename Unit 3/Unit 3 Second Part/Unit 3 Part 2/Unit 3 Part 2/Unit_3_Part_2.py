96f7ba453a258dfed605dc86cb8abbe501306cd1
Chase-Bennett <bennettchase638@gmail.com>
2024-12-16 10:07:43 -0500

    Update Unit_3_Part_2.py

--- Unit 3/Unit 3 Second Part/Unit 3 Part 2/Unit 3 Part 2/Unit_3_Part_2.py
+++ Unit 3/Unit 3 Second Part/Unit 3 Part 2/Unit 3 Part 2/Unit_3_Part_2.py
@@ -25,9 +25,40 @@ print(numbers)
 print("Source file edit test")
 print(".sln edit test")
 
-
 numbers = [10, 5, 7, 2, 1]
-print("Original list contents:", numbers)  # Printing original list contents.
- 
+print("Original list content:", numbers)  # Printing original list content.
+
 numbers[0] = 111
-print("New list contents: ", numbers)  # Current list contents.
+print("\nPrevious list content:", numbers)  # Printing previous list content.
+
+numbers[1] = numbers[4]  # Copying value of the fifth element to the second.
+print("Previous list content:", numbers)  # Printing previous list content.
+
+print("\nList's length:", len(numbers))  # Printing previous list length.
+
+###
+
+del numbers[1]  # Removing the second element from the list.
+print("New list's length:", len(numbers))  # Printing new list length.
+print("\nNew list content:", numbers)  # Printing current list content.
+
+###
+
+
+
+
+numbers = [111, 7, 2, 1]
+print(numbers[-2])
+
+
+
+hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
+
+# Step 1: write a line of code that prompts the user
+# to replace the middle number with an integer number entered by the user.
+hat_list[2]=int(input("Num: "))
+# Step 2: write a line of code that removes the last element from the list.
+del hat_list[4]
+# Step 3: write a line of code that prints the length of the existing list.
+print(len(hat_list))
+print(hat_list)
