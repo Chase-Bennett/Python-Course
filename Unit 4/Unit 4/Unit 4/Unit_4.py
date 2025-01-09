4a713fe48bb79be09322807147776f3595d46cbb
Chase-Bennett <bennettchase638@gmail.com>
2025-01-09 10:59:45 -0500

    Initial Functions

--- Unit 4/Unit 4/Unit 4/Unit_4.py
+++ Unit 4/Unit 4/Unit 4/Unit_4.py
@@ -1,3 +1,35 @@
+"""
+print("Enter a value: ")
+a = int(input())
 
-print("test")
+print("Enter a value: ")
+b = int(input())
+
+print("Enter a value: ")
+c = int(input())
+"""
+
+def message():
+    print("Enter a value please : ")
+message()
+a = int(input())
+message()
+b = int(input())
+message()
+c = int(input())
+
+
+
+
+
+
+
+
+def message():
+    print("Enter a value: ")
+
+ 
+print("We start here.")
+message()
+print("We end here.")
 
