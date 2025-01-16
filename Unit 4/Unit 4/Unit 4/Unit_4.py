0b728ecaa70944d50fc6e75d98064de3809e28e6
Chase-Bennett <bennettchase638@gmail.com>
2025-01-16 11:18:15 -0500

    Fibonacci and Factorials

--- Unit 4/Unit 4/Unit 4/Unit_4.py
+++ Unit 4/Unit 4/Unit 4/Unit_4.py
@@ -328,7 +328,7 @@ var = 1
 
 my_function(var)
 print(var)
-"""
+
 
 
 
@@ -357,3 +357,142 @@ def my_function(my_list_1):
 my_list_2 = [2, 3]
 my_function(my_list_2)
 print("Print #5:", my_list_2)
+
+#BMI Function
+def bmi(weight, height):
+    return weight / height ** 2
+ 
+ 
+print(bmi(52.5, 1.65))
+
+
+
+
+
+def ft_and_inch_to_m(ft, inch = 0.0):
+    return ft * 0.3048 + inch * 0.0254
+ 
+ 
+def lb_to_kg(lb):
+    return lb * 0.4535923
+ 
+ 
+def bmi(weight, height):
+    if height < 1.0 or height > 2.5 or weight < 20 or weight > 200:
+        return None
+ 
+    return weight / height ** 2
+ 
+ 
+print(bmi(weight = lb_to_kg(204), height = ft_and_inch_to_m(5,11)))
+
+# Triangle Formula
+
+def is_a_triangle(a, b, c):
+    if a + b <= c:
+        return False
+    if b + c <= a:
+        return False
+    if c + a <= b:
+        return False
+    return True
+
+
+print(is_a_triangle(1, 1, 1))
+print(is_a_triangle(1, 1, 3))
+
+
+
+
+# compacted verison 
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+ 
+ 
+print(is_a_triangle(1, 1, 1))
+print(is_a_triangle(1, 1, 3))
+ 
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+
+a = float(input('Enter the first side\'s length: '))
+b = float(input('Enter the second side\'s length: '))
+c = float(input('Enter the third side\'s length: '))
+
+if is_a_triangle(a, b, c):
+    print('Yes, it can be a triangle.')
+else:
+    print('No, it can\'t be a triangle.')
+
+
+
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+ 
+ 
+def is_a_right_triangle(a, b, c):
+    if not is_a_triangle(a, b, c):
+        return False
+    if c > a and c > b:
+        return c ** 2 == a ** 2 + b ** 2
+    
+    if a > b and a > c:
+        return a ** 2 == b ** 2 + c ** 2
+print(is_a_right_triangle(5, 3, 4))
+print(is_a_right_triangle(1, 3, 4))
+
+
+
+
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+ 
+ 
+def heron(a, b, c):
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+ 
+ 
+def area_of_triangle(a, b, c):
+    if not is_a_triangle(a, b, c):
+        return None
+    return heron(a, b, c)
+ 
+ 
+print(area_of_triangle(1., 1., 2. ** .5))
+
+
+def factorial_function(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+ 
+    product = 1
+    for i in range(2, n + 1):
+        product *= i
+    return product
+ 
+ 
+for n in range(1,int( input())): # testing
+    print(n, factorial_function(n))
+"""
+
+def fib(n):
+    if n < 1:
+        return None
+    if n < 3:
+        return 1
+ 
+    elem_1 = elem_2 = 1
+    the_sum = 0
+    for i in range(3, n + 1):
+        the_sum = elem_1 + elem_2
+        elem_1, elem_2 = elem_2, the_sum
+    return the_sum
+ 
+ 
+for n in range(1, 100): # testing
+    print(n, "->", fib(n))
+ 
