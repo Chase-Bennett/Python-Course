179e4efd4c7987d8e1dd91d0b3427044579de066
Chase-Bennett <bennettchase638@gmail.com>
2025-01-23 11:16:00 -0500

    4.6 Tuples and Dictionaries

--- Unit 4/Unit 4/Unit 4/Unit_4.py
+++ Unit 4/Unit 4/Unit 4/Unit_4.py
@@ -531,7 +531,7 @@ print(my_tuple[:-2])
 
 for elem in my_tuple:
     print(elem)
-"""
+
 
 
 
@@ -545,3 +545,99 @@ print(t1)
 print(t2)
 print(10 in my_tuple)
 print(-10 not in my_tuple)
+
+
+var = 123
+
+ 
+t1 = (1, )
+t2 = (2, )
+t3 = (3, var)
+ 
+t1, t2, t3 = t2, t3, t1
+ 
+print(t1, t2, t3)
+
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+phone_numbers = {'boss': 5551234567, 'Suzy': 22657854310}
+empty_dictionary = {}
+ 
+print(dictionary)
+print(phone_numbers)
+print(empty_dictionary)
+
+
+print(dictionary['cat'])
+print(phone_numbers['Suzy'])
+ 
+
+
+
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+words = ['cat', 'lion', 'horse']
+ 
+for word in words:
+    if word in dictionary:
+        print(word, "->", dictionary[word])
+    else:
+        print(word, "is not in dictionary")
+
+
+
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+ 
+for key in dictionary.keys():
+    print(key, "->", dictionary[key])
+
+
+
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+ 
+dictionary['cat'] = 'minou'
+print(dictionary)
+ 
+ 
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+ 
+dictionary['swan'] = 'cygne'
+print(dictionary)
+
+
+
+dictionary.update({"duck": "canard"})
+print(dictionary)
+
+
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+ 
+dictionary.popitem()
+print(dictionary) # outputs: {'cat': 'chat', 'dog': 'chien'}
+"""
+school_class = {}
+
+while True:
+    name = input("Enter the student's name: ")
+    if name == '':
+        break
+    
+    score = int(input("Enter the student's score (0-10): "))
+    if score not in range(0, 11):
+	    break
+    
+    if name in school_class:
+        school_class[name] += (score,)
+    else:
+        school_class[name] = (score,)
+        
+for name in sorted(school_class.keys()):
+    adding = 0
+    counter = 0
+    for score in school_class[name]:
+        adding += score
+        counter += 1
+    print(name, ":", adding / counter)
